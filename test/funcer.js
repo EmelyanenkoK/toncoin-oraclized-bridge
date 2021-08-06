bec21acc3164947b1c2c0b95ac13e4c24249d6e3
tolya-yanot <tolyayanot@gmail.com>
2021-08-06 17:54:46 +0300

    folders

---
+++ test/funcer.js
@@ -0,0 +1,276 @@
+const {exec} = require('child_process');
+const fs = require('fs')
+
+const makeType = (key) => {
+    if (key === 'coins') {
+
+        return 'Gram,';
+
+    } else if (key === 'address') {
+
+        return 'parse-smc-addr drop addr,'
+
+    } else if (key.startsWith('uint')) {
+
+        const bits = key.substr(4);
+        if (!Number(bits)) throw new Error('invalid uint bits "' + bits + '"');
+        return bits + ' u,';
+
+    } else if (key.startsWith('int')) {
+
+        const bits = key.substr(3);
+        if (!Number(bits)) throw new Error('invalid int bits "' + bits + '"');
+        return bits + ' i,';
+
+    } else {
+        throw new Error('unsupported type "' + key + '"');
+    }
+}
+
+const makeValue = (key, value) => {
+    if (key === 'address') {
+        return '"' + value + '"';
+    } else {
+        return value;
+    }
+}
+
+const makeMapKey = (key, value) => {
+    if (key.startsWith('uint')) {
+
+        const bits = key.substr(4);
+        if (!Number(bits)) throw new Error('invalid uint bits "' + bits + '"');
+        return value + ' rot ' + bits + ' udict! drop';
+
+    } else if (key.startsWith('int')) {
+
+        const bits = key.substr(3);
+        if (!Number(bits)) throw new Error('invalid int bits "' + bits + '"');
+        return value + ' rot ' + bits + ' idict! drop';
+
+    } else {
+        throw new Error('unsupported map type "' + key + '"');
+    }
+}
+
+const makeMap = (key, value) => {
+    const mapIndex = key.indexOf('->');
+    const mapKey = key.substr(0, mapIndex);
+    const mapValueKey = key.substr(mapIndex);
+
+    let s = 'dictnew\n';
+    let i = 0;
+    for (let _key in value) {
+        const _value = value[_key];
+        s += makeParams(_value) + ' <s ' + makeMapKey(mapKey, _key) + '\n';
+        i += 2;
+    }
+
+    return s;
+}
+
+const makeParam = (key, value) => {
+    if (key.indexOf('->') > -1) {
+        return makeMap(key, value) + ' dict,';
+    }
+
+    if (key === 'cell') {
+        return makeParams(value) + ' ref,';
+    }
+
+    return makeValue(key, value) + ' ' + makeType(key);
+}
+
+const makeParams = (arr) => {
+    let s = '<b\n';
+    let i = 0;
+    while (i < arr.length) {
+        const key = arr[i];
+        const value = arr[i + 1];
+        s += makeParam(key, value) + '\n';
+        i += 2;
+    }
+    s += 'b>';
+    return s;
+}
+
+const makeInMsg = (i, inMsg) => {
+    return `
+"${inMsg.sender}" // sender address
+parse-smc-addr drop 2constant sender_address${i}
+
+${inMsg.amount} constant msg_value${i} // in_msg amount
+
+${makeParams(inMsg.body)} constant in_msg_body${i}
+
+<b b{0110} s,
+   sender_address${i} Addr,
+   // TODO: add other data
+b> constant in_msg${i}
+
+0x076ef1ea           // magic
+0                    // actions
+0                    // msgs_sent
+1628090356           // unixtime
+1                    // block_lt
+1                    // trans_lt
+239                  // randseed
+1000000000 null pair // balance_remaining
+contract_address     // myself
+global_config        // global_config
+10 tuple 1 tuple constant c7
+
+msg_value${i} in_msg${i} in_msg_body${i} <s 0 code storage c7 0x75 runvmx
+// returns ...values exit_code new_c4 c5
+
+${inMsg.new_data ? makeCheckStorage(inMsg.new_data) : ''}
+
+${inMsg.out_msgs ? makeCheckOutMessages(inMsg.out_msgs) : ''}
+`;
+}
+
+const makeCheckStorage = (newStorage) => {
+    return `
+${makeParams(newStorage)} constant correct_new_storage
+
+rot { ."Error: non-zero exit code" cr 1 halt } if
+swap hashu correct_new_storage hashu <> { ."Error: incorrect resulting storage" cr 2 halt } if   
+`;
+}
+
+const makeCheckOutMessages = (outMessages) => {
+    return `
+<s dup 0 swap { dup empty? not } {
+  ref@ <s swap 1+ swap
+} while drop
+${outMessages.length} <> { ."Error: incorrect number of actions" cr 3 halt } if
+
+{ rot = -rot = and } : 2=
+{ 2= not } : 2<>
+
+${outMessages.reverse().map(outMsg => makeCheckOutMsg(outMsg)).join('\n')}
+    `
+}
+
+
+const makeCheckOutMsg = (outMsg) => {
+    return `
+4 B@+ swap B{0EC3C86D} B= not abort"Unsupported action"
+8 u@+ swap =: send-mode
+ref@+ <s swap ref@ <s parse-msg
+
+msg.body hashu ${makeParams(outMsg.body)} hashu <> { ."Error: incorrect message body" cr 8 halt } if
+"${outMsg.to}" parse-smc-addr drop msg.dest 2<> { ."Error: incorrect message destination" cr 9 halt } if
+msg.value ${outMsg.amount} <> { ."Error: incorrect message value" cr 10 halt } if
+send-mode ${outMsg.sendMode || 3} <> { ."Error: incorrect message sendmode" cr 11 halt } if
+`
+}
+
+const makeInMessages = (inMsgs) => {
+    let s = '';
+    for (let i in inMsgs) {
+        s += makeInMsg(i, inMsgs[i]);
+    }
+    return s;
+}
+
+const makeConfigParams = (configParams) => {
+    return makeMap('int32->any', configParams);
+}
+
+const makeTestFif = (data) => {
+    return `
+"TonUtil.fif" include
+"Asm.fif" include
+
+// s -- wc addr s'
+{ 1 i@+ swap not abort"Internal address expected"
+  1 i@+
+  1 i@+ swap { 4 u@+ swap u@+ nip } if
+  swap { 9 u@+ 32 } { 256 swap 8 } cond
+  i@+ rot u@+
+} : addr@+
+{ addr@+ drop } : addr@
+
+// s --
+{
+  1 i@+ swap { ."not an internal message" cr 1 halt } if
+  1 i@+ swap =: msg.ihr-disabled
+  1 i@+ swap =: msg.bounce
+  1 i@+ nip
+  2 u@+ swap 0 <> { ."src = none expected" cr 1 halt } if
+  addr@+ -rot 2=: msg.dest
+  Gram@+ swap =: msg.value
+  1 i@+ swap { ref@+ swap } { null } cond =: msg.extra
+  Gram@+ nip Gram@+ nip
+  64 u@+ nip 32 u@+ nip
+  1 i@+ swap abort"StateInit is not supported"
+  1 i@+ swap { ref@ } { s>c } cond =: msg.body
+} : parse-msg
+
+// <{ SETCP0 CONT:<{ c5 PUSH }> ATEXIT
+//      "compiled.fif" include <s @addop
+// }>s constant code
+"compiled.fif" include <s constant code
+
+${makeParams(data.data)} constant storage
+
+${makeConfigParams(data.configParams)} constant global_config
+
+<b <b b{00110} s, <b code s, b> ref, storage ref, b>
+hashu -1 swap addr, b> constant contract_address
+
+${makeInMessages(data.in_msgs)}
+`;
+}
+
+const funcer = (data) => {
+    const func = data.func;
+    const fift = data.fift;
+    const path = data.path;
+
+    const compileFuncCmd = func + ' -SP ' + ' -o ' + path + 'compiled.fif ' + data.fc.map(fc => path + fc).join(' ');
+    const runFiftCmd = fift + ' ' + path + 'test.fif';
+    const testFif = makeTestFif(data);
+
+    console.log(compileFuncCmd);
+    console.log(testFif);
+    console.log(runFiftCmd);
+
+
+    exec(compileFuncCmd, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            // node couldn't execute the command
+            return;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+
+        fs.writeFile(path + 'test.fif', testFif, err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            console.log('test.fif OK')
+
+            exec(runFiftCmd, (err, stdout, stderr) => {
+                if (err) {
+                    console.error(err);
+                    // node couldn't execute the command
+                    return;
+                }
+
+                // the *entire* stdout and stderr (buffered)
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+            });
+        })
+    });
+
+
+}
+
+module.exports = {funcer};
---
+++ test/index.js
@@ -0,0 +1,87 @@
+const {funcer} = require("./funcer");
+
+// export FIFTPATH=/Users/tolyayanot/dev/newton/ton/crypto/fift/lib:/Users/tolyayanot/dev/newton/ton/crypto/
+
+funcer({
+    'fift': '/Users/tolyayanot/dev/newton/liteclient-build/crypto/fift',
+    'func': '/Users/tolyayanot/dev/newton/liteclient-build/crypto/func',
+    'path': '/Users/tolyayanot/dev/newton/func_bridge/',
+    'fc': [
+        'stdlib.fc',
+        'nonstdlib.fc',
+        'text_utils.fc',
+        'message_utils.fc',
+        'fee_utils.fc',
+        'storage.fc',
+        'bridge_code.fc',
+        'get_methods.fc',
+    ],
+    "configParams": {
+        71: [
+            'cell', [
+                "coins", "1000000000", // flat_stake
+                "uint16", 1, // min_auth_num
+                "uint32", 0, //seqno
+                "uint16->any", { // oracles
+                    "0": [
+                        "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8"
+                    ]
+                }
+            ]
+        ]
+    },
+    'data': [
+        "coins", 0, // total_staked
+        "coins", 0, // total_locked
+        "uint256", 0, // current_set_hash
+        "uint256->any", {}, // sets
+        "uint256->any", {}, // candidates
+        "uint256->any", {}, // external_votings
+        "uint256->any", {}, // ton_votings
+    ],
+    'in_msgs': [
+        {
+            "sender": "-1:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
+            "amount": "10000000000",
+            "body": [
+                "uint32", 1, // op  deposit stake
+                "uint64", 123, // query_id
+                "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", // ed_publey
+                "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8" // secp_pubkey
+            ],
+            "new_data": [
+                "coins", 0, // total_staked
+                "coins", 0, // total_locked
+                "uint256", 0, // current_set_hash
+                "uint256->any", {}, // sets
+                "uint256->any", {}, // candidates
+                "uint256->any", {}, // external_votings
+                "uint256->any", {}, // ton_votings
+            ],
+            "out_msgs": [
+                {
+                    "to": "-1:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
+                    "amount": "10000000000",
+                    "sendMode": 64,
+                    "body": [
+                        "uint32", 1, // op  deposit stake
+                        "uint64", 123, // query_id
+                        "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", // ed_publey
+                        "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8" // secp_pubkey
+                    ],
+                },
+                {
+                    "to": "-1:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
+                    "amount": "10000000000",
+                    "sendMode": 64,
+                    "body": [
+                        "uint32", 1, // op  deposit stake
+                        "uint64", 123, // query_id
+                        "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", // ed_publey
+                        "uint256", "0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8" // secp_pubkey
+                    ],
+                },
+            ]
+        },
+    ]
+});
