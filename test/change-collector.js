cdaa32e8c4e38f642f9f0cf4c448a6d8fd8a2eb9
akifoq <akifoq@protonmail.com>
2021-08-13 21:12:16 +0300

    Cosmetic in tests

--- test/change-collector.js
+++ test/change-collector.js
@@ -23,9 +23,7 @@ funcer({}, {
     'data': [
         "uint8", 0, // state_flags
         "coins", 0, // total_locked
-        "uint3", 4, // collector_address, prefix
-        "int8", 0, // collector_address, wc
-        "uint256", "0xe53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
+        "Address", "0:e53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
         "coins", 2*1e9, // flat_reward
         "coins", 3*1e9, // network_fee
         "uint14", 0, // factor
@@ -42,9 +40,7 @@ funcer({}, {
             "new_data": [
                 "uint8", 0, // state_flags
                 "coins", 0, // total_locked
-                "uint3", 4, // collector_address, prefix
-                "int8", -1, // collector_address, wc
-                "uint256", "0x77dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", // collector_address
+                "Address", "-1:77dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", // collector_address
                 "coins", 2*1e9, // flat_reward
                 "coins", 3*1e9, // network_fee
                 "uint14", 0, // factor
--- test/change-fee.js
+++ test/change-fee.js
@@ -23,9 +23,7 @@ funcer({}, {
     'data': [
         "uint8", 0, // state_flags
         "coins", 0, // total_locked
-        "uint3", 4, // collector_address, prefix
-        "int8", 0, // collector_address, wc
-        "uint256", "0xe53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
+        "Address", "0:e53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
         "coins", 2*1e9, // flat_reward
         "coins", 3*1e9, // network_fee
         "uint14", 0, // factor
@@ -44,9 +42,7 @@ funcer({}, {
             "new_data": [
                 "uint8", 0, // state_flags
                 "coins", 0, // total_locked
-                "uint3", 4, // collector_address, prefix
-                "int8", 0, // collector_address, wc
-                "uint256", "0xe53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
+                "Address", "0:e53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
                 "coins", 1*1e9, // flat_reward
                 "coins", 2*1e9, // network_fee
                 "uint14", 3, // factor
