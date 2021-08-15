c2533a47cac71855516baa64fad90eda84b6fe00
tolya-yanot <tolyayanot@gmail.com>
2021-08-15 18:54:14 +0300

    test fee

--- test/ton2eth.js
+++ test/ton2eth.js
@@ -7,7 +7,7 @@ const makeStorage = (totalLocked) => {
         "uint3", 4, // collector_address, prefix
         "int8", 0, // collector_address, wc
         "uint256", "0xe53bddefb065373732ec25d5f9af0b3f7a3be358ea87ec285b4b6330a67d8c6a", // collector_address
-        "coins", 2*1e9, // flat_reward
+        "coins", 1*1e9, // flat_reward
         "coins", 3*1e9, // network_fee
         "uint14", 200, // factor
     ];
@@ -42,7 +42,7 @@ funcer({},{
             "body": [
                 "comment", `swapTo#${ethAddress}`
             ],
-            "new_data": makeStorage(5*1e9*0.98),
+            "new_data": makeStorage(6*1e9*0.98),
             "out_msgs": [
                 {
                     "type": "External",
@@ -50,7 +50,7 @@ funcer({},{
                     "sendMode": 2,
                     "body": [
                         "uint160", ethAddress, // destination_address
-                        "uint64", 5*1e9*0.98 // amount - fees
+                        "uint64", 6*1e9*0.98 // amount - fees
                     ],
                 },
                 {
@@ -59,7 +59,7 @@ funcer({},{
                     "amount": 100000000,
                     "sendMode": 2,
                     "body": [
-                      "comment", `OK ${5*1e9*0.98} `
+                      "comment", `OK ${6*1e9*0.98} `
                     ],
                 }
             ]
