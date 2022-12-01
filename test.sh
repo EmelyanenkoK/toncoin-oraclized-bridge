6dfb7b38f86619783aafae0dee3cf580e9a056b4
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-12-01 04:00:31 +0300

    test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+node test/change-collector.js &&
+node test/change-fee.js &&
+node test/change-fee-not-oracle.js &&
+node test/change-state.js &&
+node test/eth2ton.js &&
+node test/get-reward.js &&
+node test/index.js &&
+node test/migrate.js &&
+node test/ton2eth.js &&
+node test/ton2eth-binary.js &&
+node test/ton2eth-disabled.js &&
+echo "OK"
